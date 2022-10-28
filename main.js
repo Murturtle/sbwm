0840805d7b31705fc2f0aa67812bebba2f95f2cc
Murturtle <97775022+Murturtle@users.noreply.github.com>
2022-10-28 14:03:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-if (document.getElementById('mymenu')) {
-    document.getElementById('mymenu').remove();
+if (document.getElementById('sidemenu')) {
+    document.getElementById('sidemenu').remove();
 } else {
-    var block_to_insert;
-    var container_block;
-    block_to_insert = document.createElement('div');
-    block_to_insert.id = 'mymenu';
-    block_to_insert.innerHTML = `
+    var sidemenu;
+    var body;
+    sidemenu = document.createElement('div');
+    sidemenu.id = 'sidemenu';
+    sidemenu.innerHTML = `
     
     <center><b style="font-size:16px;">SBWM</b>
     <br>
     <button onclick="window.open('https://ee.wrya.net/leaderboards/')">EE</button>
-    <p id="attacks">
     <p id="key">
     
     </center>
@@ -25,9 +24,9 @@ if (document.getElementById('mymenu')) {
     </style>
     `;
     
-    container_block = document.getElementsByTagName('body')[0];
-    container_block.appendChild(block_to_insert);
-    mymenu.setAttribute('style', 'height:25%; width:80px; background-color:rgba(0,0,0,.5); color:white; float:right; font-family:Courier New; font-size:12px; z-index:10000; display: inline-block; overflow:visible; position:fixed; top: 25%;');
+    body = document.getElementsByTagName('body')[0];
+    body.appendChild(sidemenu);
+    mymenu.setAttribute('style', 'height:25%; width:80px; background-color:rgba(0,0,0,.5); color:white; font-family:Courier New; font-size:12px; z-index:10000; display: inline-block; overflow:visible; position:fixed; top: 25%;');
 }
 
 var attacks=0;
@@ -35,8 +34,23 @@ var attacks=0;
 document.onkeydown = function (e) {
     switch (e.keyCode) {
     case 81:
-        attacks+=1;
         document.getElementById('attacks').innerHTML=attacks;
+    case 16:
+        if (document.getElementById('mainmenu')) {
+        document.getElementById('mainmenu').remove();
+        } else {
+            var sidemenu;
+            var body;
+            mainmenu = document.createElement('div');
+            mainmenu.id = 'mainmenu';
+            mainmenu.innerHTML = `
+            `;
+            
+            body = document.getElementsByTagName('body')[0];
+            body.appendChild(mainmenu);
+            mainmenu.setAttribute('style', 'height:90%; width:90%; background-color:rgba(0,0,0,.5); color:white; font-family:Courier New; font-size:12px; z-index:10000; display: inline-block; overflow:visible; position:fixed; top: 5%;');
+        }
+      
     default:
         document.getElementById('key').innerHTML=e.code;
   }
